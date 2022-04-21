de848663a99d5b8fdd8b7155ac291274bbac3daa
Meshachcomlab <102945891+Meshachcomlab@users.noreply.github.com>
2022-04-21 15:36:06 +0530

    Create cartons_breakUP.java

---
+++ cartons_breakUP.java
@@ -0,0 +1,19 @@
+// ex:5
+// Cartons Break-Up
+/*
+A company manufactures packing cartons in 4 sizes i.e. cartons to accommodate 6 boxes,12 boxes,24 boxes, 48 boxes. Design a 
+program to accept the number of boxes to be packed (N) by the user (max upto 1000 boxes) 
+and display the break-up of cartons used in descending order of capacity 
+(i.e. preference should be given to the highest capacity available and if boxes
+are left are less than 6 an extra carton of capacity of 6 should be used. )
+Example 1 Input N = 726 
+Output: 48*15 = 720 
+6*1 = 6
+Remaining boxes = 0
+Total number of boxes = 726 
+Total number of cartons = 16
+*/
+import java.util.*;
+public class cartons_breakUP {
+    
+}
