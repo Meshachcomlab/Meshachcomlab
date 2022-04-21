a74544b87f0357237c0049afab51777dd148ea66
Meshachcomlab <102945891+Meshachcomlab@users.noreply.github.com>
2022-04-21 15:14:53 +0530

    uploaded

--- isbn.java
+++ isbn.java
@@ -1,3 +1,25 @@
+// ex:1
+// isbn
+/*
+Aim:Develop a program to accept a ten-digit code and check whether it's a valid isbn code
+
+ Algorithm:
+  1.  start
+  2.  Input m and n 
+  3.  If(m<1 or n<1 or m>n) then print "INVALID INPUT" and goto step 20
+  4.  count = 0
+  5.  Initialize a,sum and i as 0 and k as 10
+  6.  Set a loop i from m to n and repeat steps 7 to 10
+  7.  Seperate i^th character of s and assign to ch
+  8.  if(ch=="x") then a =10 else a = integer value of ch
+  9.  sum = sum + a*k;
+  10. k-=1
+  11. Print sum
+  12. if(sum%11 == 0 ) then print "Leaves No Remainder - Valid ISBN CODE";
+      else Print "Leaves Remainder -INVALID ISBN CODE"
+  13. Stop
+*/
+
 import java.util.*;
 
 public class isbn {
@@ -6,7 +28,6 @@ public class isbn {
         System.out.println("Enter the isbn code");
         String s = in.nextLine();
         int l = s.length();
-        System.out.println("Invalid code");
         if (l != 10) {
             System.out.println("Invalid input");
         } else {
@@ -18,17 +39,26 @@ public class isbn {
         String ch;
         int a = 0, sum = 0, k = 10, l = s.length();
         for (int i = 0; i < l; i++) {
-           ch = Character.toString(s.charAt(i));
-           if(ch.equalsIgnoreCase("X")){
-               a = 10;
-           }
-           else{
-               
-           }
+            ch = Character.toString(s.charAt(i));
+            if (ch.equalsIgnoreCase("X")) {
+                a = 10;
+            } else {
+                a = Integer.parseInt(ch);
+                sum = sum + a * k;
+                k--;
+            }
+           
+        }
+        System.out.println("OUTPUT: Sum =" + sum);
+        if (sum % 11 == 0) {
+            System.out.println("Leaves No Remainder -VALID ISBN CODE");
+        } else {
+            System.out.println("Leaves Remainder -INVALID ISBN CODE");
         }
     }
 
     public static void main(String[] args) {
-
+        isbn ib = new isbn();
+        ib.input();
     }
 }
--- smallest_int.java
+++ smallest_int.java
@@ -1,3 +1,35 @@
+// ex:3
+// Smallest Integer
+/*
+    Aim: Develop a program to accept 2 +ve numbers M and N Such that M is between  
+     100 and 10000 and N is less than 100 and print the smallest required number 
+     whose sum of all it's digits is equal to N. Also print the total number of digits
+     present in the required number.
+     
+    Algorithm:
+    1.  start
+    2.  Input M and N
+    3.  if(M<1000 or m>10000 or n<1 or n>99) then print "INVALID INPUT" and goto step 9
+    4.  initialize i as m
+    5.  Repeat step 6 till the method sumDig(i) != n
+    6.  i+=1
+    7.  Print the required number as i
+    8.  Call the method to countDig(i)     
+    9. stop
+
+    sumDig(int n)
+    1. sum = 0
+    2. Repeat Steps 3 to 5 till n > 0
+    3. d = n%10
+    4. sum += d
+    5. n/=10
+    6. return Sum
+
+    countDig(int n)
+    1. Convert n to String s
+    2. Find the length of s and assign to len
+    return len
+ */
 import java.util.*;
 class smallest_int {
     int m, n;
