f75e73c770064a4731b251ac345a289a52bd927e
Meshachcomlab <102945891+Meshachcomlab@users.noreply.github.com>
2022-11-03 15:36:23 +0530

    a

---
+++ caesar_cipher.java
@@ -0,0 +1,35 @@
+
+// ex:20
+import java.util.*;
+
+public class caesar_cipher {
+    String str = "";
+    String nstr = "";
+
+    void input() {
+        Scanner in = new Scanner(System.in);
+        str = in.nextLine();
+        if (str.length() > 3 && str.length() < 100) {
+            proces();
+        }
+    }
+
+    void proces() {
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isLetter(str.charAt(i))) {
+                nstr+=(char) (str.charAt(i)+103);
+            }
+            else{
+                nstr+=str.charAt(i);
+            }
+        }
+        System.out.println(nstr);
+    }
+
+    public static void main(String[] args) {
+        caesar_cipher obj20 = new caesar_cipher();
+        System.out.println("INPUT:");
+        // obj20.input();
+        System.out.println('a'+102);
+    }
+}
--- words_vowels.java
+++ words_vowels.java
@@ -12,29 +12,44 @@ public class words_vowels {
         sen = in.nextLine();
         if (sen.charAt(sen.length() - 1) == '.' || sen.charAt(sen.length() - 1) == '!'
                 || sen.charAt(sen.length() - 1) == '?') {
-            sen_ar = sen.split(" ");
+            sen_ar = sen.split("[.?! ]+");
+        } else {
+            System.out.println("INVALID INPUT!");
+            input();
         }
     }
 
     void process() {
-        String vowels;
+        int count = 0;
+        String vowels = "", nvowels = "";
         char[] v = { 'A', 'E', 'I', 'O', 'U' };
+        boolean state = true;
         for (int i = 0; i < sen_ar.length; i++) {
+            state = true;
             for (int j = 0; j < v.length; j++) {
-                if (sen_ar[i].charAt(0) == v[j] && sen_ar[i].charAt(sen_ar[i].length()) == v[j]) {
-
+                if (sen_ar[i].charAt(0) == v[j]) {
+                    for (int k = 4; k >= 0; k--) {
+                        if (sen_ar[i].charAt(sen_ar[i].length() - 1) == v[k]) {
+                            vowels += sen_ar[i] + " ";
+                            count++;
+                            state = false;
+                        }
+                    }
+                }
+                if (state) {
+                    nvowels += sen_ar[i] + " ";
+                    break;
                 }
             }
         }
-        for (String string : sen_ar) {
-            System.out.println(string);
-        }
+        System.out.println(vowels + nvowels);
+        System.out.println("NUMBER OF WORDS BEGINNING AND ENDING WITH A VOWEL = " + count);
     }
 
     public static void main(String[] args) {
         words_vowels obj19 = new words_vowels();
         obj19.input();
+        System.out.println("OUTPUT:");
         obj19.process();
-
     }
 }
